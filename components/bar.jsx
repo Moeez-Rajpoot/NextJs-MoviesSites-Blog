ae11b8541200a3ed16aa297a21603c3c5ee53d1f
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-09 14:45:32 +0500

    Updated

--- components/bar.jsx
+++ components/bar.jsx
@@ -8,8 +8,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 export default function bar() {
   return (
-    <div className="bg-white relative h-14 flex w-full sm:w-[90%] lg:w-[72%] mb-96">
-      <div className="hidden sm:w-1/2 lg:w-[33.33%] h-full sm:flex items-center bg-green-500">
+    <div className=" relative h-14 flex w-full sm:w-[90%] lg:w-[72%] mb-96">
+      <div className="hidden sm:w-1/2 lg:w-[33.33%] h-full sm:flex items-center ">
         <Image
           alt="icon"
           src="https://static1.makeuseofimages.com/wordpress%2Fwp-content%2Fauthors%2F5fc785706f3fb-IMG_20200916_155509.jpg?fit=crop&w=90&h=90"
@@ -24,14 +24,14 @@ export default function bar() {
         </p>
       </div>
 
-      <div className="w-full sm:w-1/2  lg:w-[33.33%] h-full flex justify-between lg:justify-center items-center bg-blue-500">
+      <div className="w-full sm:w-1/2  lg:w-[33.33%] h-full flex justify-between lg:justify-center items-center">
         <div className="flex items-center ml-3 sm:ml-0 text-white hover:cursor-pointer  hover:text-red-700 justify-center h-full">
           <FontAwesomeIcon
             className=" hover:text-red-700"
             icon={faComments}
           ></FontAwesomeIcon>
           <p className="text-xs ml-1 ">Threads</p>
-          <div className="px-2 py-1 font-bold text-xs ml-1 rounded-full bg-gray-600">
+          <div className="px-2 py-[2px] font-bold text-xs ml-1 rounded-full bg-gray-600">
             1
           </div>
         </div>
@@ -55,9 +55,9 @@ export default function bar() {
         </div>
       </div>
 
-      <div className="w-[33.33%] hidden lg:block  h-full bg-yellow-500">
+      <div className="w-[33.33%] hidden lg:block  h-full">
         <div className="flex gap-1 justify-end mr-1 items-center h-full">
-          <p className="font-bold text-sm ">Sign In Into Your <span className="text-red-700">MUO</span> Account</p>
+          <p className="font-bold text-sm hover:cursor-pointer hover:underline ">Sign In Into Your <span className="text-red-700">MUO</span> Account</p>
           <div className="rounded-full bg-white ml-1 px-[5px] py-[5px] flex justify-center items-center">
             <FontAwesomeIcon className="text-black h-3 w-3" icon={faUser}></FontAwesomeIcon>
 
