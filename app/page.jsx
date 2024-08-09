26f7dcf1c2d2ed58f5d2f477762b08eb3d4049a0
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-09 13:06:55 +0500

    Updated

--- app/page.jsx
+++ app/page.jsx
@@ -27,18 +27,22 @@ export default function Home() {
             <span className="relative -left-3 bottom-[1px]">Avoiding Scam</span>
           </li>
           <li className="text-nowrap hover:underline hover:cursor-pointer marker:text-gray-600 marker:text-xl">
-            <span className="relative -left-3 bottom-[1px]">Emojis Explained</span>
+            <span className="relative -left-3 bottom-[1px]">
+              Emojis Explained
+            </span>
           </li>
           <li className="text-nowrap hover:underline hover:cursor-pointer marker:text-gray-600 marker:text-xl">
-            <span className="relative -left-3 bottom-[1px]">Free Movies Streaming</span>
+            <span className="relative -left-3 bottom-[1px]">
+              Free Movies Streaming
+            </span>
           </li>
         </div>
       </div>
 
       <h1 className=" flex md:hidden text-2xl mt-3 px-5 sm:px-9  font-bold p text-white">
-          10 Free Movie Streaming Sites With No Sign Up Requirements
-        </h1>
-        <div className="w-full px-5 sm:px-9  mt-2 md:hidden h-full flex items-center">
+        10 Free Movie Streaming Sites With No Sign Up Requirements
+      </h1>
+      <div className="w-full px-5 sm:px-9  mt-2 md:hidden h-full flex items-center">
         <Image
           alt="icon"
           src="https://static1.makeuseofimages.com/wordpress%2Fwp-content%2Fauthors%2F5fc785706f3fb-IMG_20200916_155509.jpg?fit=crop&w=90&h=90"
@@ -48,14 +52,13 @@ export default function Home() {
         ></Image>
 
         <p className="text-xs font-semibold cursor-pointer ml-1">
-          By <span className="hover:text-red-500 text-xs">Simona Tolcheva </span>{" "}
-          &mdash; <span className="text-xs text-[#848a91">  May 14, 2024 </span>{" "}
+          By{" "}
+          <span className="hover:text-red-500 text-xs">Simona Tolcheva </span>{" "}
+          &mdash; <span className="text-xs text-[#848a91"> May 14, 2024 </span>{" "}
         </p>
       </div>
       {/* Image Section */}
       <div className="w-full sm:w-[90%] lg:w-[72%] h-[200px] sm:h-[320px] lg:h-[610px] relative mt-5 bg-white flex flex-col justify-center overflow-hidden items-center">
-    
-    
         <Image
           src={bgpic}
           alt="A person holding a remote and choosing between different screens"
@@ -69,14 +72,8 @@ export default function Home() {
         </h1>
       </div>
 
-
-
       {/* bar */}
-      <Bar/>
-
-
-
-
+      <Bar />
     </main>
   );
 }
