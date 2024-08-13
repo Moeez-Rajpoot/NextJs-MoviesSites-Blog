cdaa97d18d13e39eb55da36915272947a6339f7d
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-13 11:45:22 +0500

    Updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/navbar";
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics/>
         <SpeedInsights />
         </body>
     </html>
